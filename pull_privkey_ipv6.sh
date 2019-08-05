36d6edf77201181ebedd58d563afe2fe251393e7
Ryul-K <dyul4440@gmail.com>
2019-08-05 17:02:17 +0900

    .

--- pull_privkey_ipv6.sh
+++ pull_privkey_ipv6.sh
@@ -16,6 +16,8 @@ COIN_PATH='/usr/bin/'
 COIN_PORT=29871
 RPC_PORT=29872
 SET_NUM=6
+POPC_CONFIG_FILE='pointofpubliccoin.conf'
+POPC_CONFIGFOLDER='/root/.pointofpubliccoincore'
 
 BLUE="\033[0;34m"
 YELLOW="\033[0;33m"
@@ -34,12 +36,12 @@ for (( i = 1; i <= $SET_NUM; i++)); do  #NODEIPv6에 포트셋팅  /etc/network/
   echo "mn_IPv6[$i] : ${mn_IPv6[$i]}"
 done
 
-function 7_pull_privkey_ipv6() {
+function macc_privkey_ipv6() {
    tmpIPv4=$(curl -s4 icanhazip.com)
 
    mn_key[0]=$(sed -n '/masternodeprivkey/p' $CONFIGFOLDER/$CONFIG_FILE)
    echo " "
-   echo " "
+   echo -e "${RED} ======== MACC PRIVATEKEY & IPV6  ========${NC}"
    echo -e "${GREEN}`hostname` ${tmpIPv4} ${mn_key[0]:18:70}\t${NC}"
 
    for (( i = 1; i <= $SET_NUM; i++)); do
@@ -52,4 +54,27 @@ function 7_pull_privkey_ipv6() {
 
 }
 
-7_pull_privkey_ipv6
+function POPC_privkey_ipv6() {
+   tmpIPv4=$(curl -s4 icanhazip.com)
+
+   mn_key[0]=$(sed -n '/masternodeprivkey/p' $POPC_CONFIGFOLDER/$POPC_CONFIG_FILE)
+   echo " "
+   echo -e "${RED} ======== POPC PRIVATEKEY & IPV6  ========${NC}"
+   echo -e "${GREEN}`hostname` ${tmpIPv4} ${mn_key[0]:18:70}\t${NC}"
+
+   for (( i = 1; i <= $SET_NUM; i++)); do
+     #mn_key[$i]=$(sed -n '/masternodeprivkey/p' $CONFIGFOLDER$i/$CONFIG_FILE
+     mn_key[$i]=$(sed -n '/masternodeprivkey/p' $POPC_CONFIGFOLDER$i/$POPC_CONFIG_FILE)
+     echo -e "${GREEN}`hostname`-$i ${mn_IPv6[$i]} ${mn_key[$i]:18:70}\t${NC}"
+   done
+   echo " "
+   echo " "
+
+
+
+
+}
+
+
+POPC_privkey_ipv6
+macc_privkey_ipv6
